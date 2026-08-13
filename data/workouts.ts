782be757ea7dff8faacb78b90a0f64ba9e5b7e7f
Sofia Runsten <runsten.sofia@gmail.com>
2026-08-13 13:50:39 +0200

    added data and types

---
+++ data/workouts.ts
@@ -0,0 +1,34 @@
+import { Workout } from '../types/workout'
+
+export const workouts: Workout[] = [
+  {
+    id: 1,
+    name: "Sit Ups",
+    workTime: 60,
+  },
+  {
+    id: 2,
+    name: "Squats",
+    workTime: 45,
+  },
+  {
+    id: 3,
+    name: "Push Ups",
+    workTime: 30,
+  },
+  {
+    id: 4,
+    name: "Jumping Jacks",
+    workTime: 30,
+  }, 
+    {
+    id: 5,
+    name: "Deadbug",
+    workTime: 60,
+  }, 
+    {
+    id: 6,
+    name: "Plank",
+    workTime: 45,
+  },
+]
---
+++ types/workout.ts
@@ -0,0 +1,5 @@
+export interface Workout {
+  id: number,
+  name: string,
+  workTime: number, 
+}
