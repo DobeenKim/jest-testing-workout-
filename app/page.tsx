ed4a9d3a5a52b018630b65b185c2030d1269ff79
Dobeen Kim <dobin7955@gmail.com>
2026-08-24 13:40:20 +0200

    finished home

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
+'use client'
 import Header from "@/component/Header";
-import WorkoutCard from "@/component/WorkoutCard";
+import ActiveWorkout from "@/component/ActiveWorkout";
 import WorkoutSelection from "@/component/workoutSelection";
-import { workouts } from "@/data/workouts";
-import { types } from "../types/workout";
+import { useState } from "react";
+import { Workout } from "@/types/workout";
+
 
 export default function Home() {
+  const [selectedWorkout, setSelectedWorkout] = useState<Workout | null>(null)
+
   return (
     <div className="">
       <Header />
-      <WorkoutCard />
+      {!selectedWorkout && (
+          <WorkoutSelection onSelect={(workout => setSelectedWorkout(workout))} />
+      )}
+      {selectedWorkout && (
+        <ActiveWorkout workout={selectedWorkout} goBack={() => setSelectedWorkout(null)}/>
+      )}
     </div>
   );
 }
