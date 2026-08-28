c64a08511f3634d2f276eb975e7de06dafe1b946
Dobeen Kim <dobin7955@gmail.com>
2026-08-28 09:54:56 +0200

    fixed error

--- __test__/Timer.test.tsx
+++ __test__/Timer.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Timer from '../component/Timer'
 import { act } from 'react'
 
